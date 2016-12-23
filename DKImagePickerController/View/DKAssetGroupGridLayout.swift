2ae32648b0ba58d36f0d449687c09e5abc19a81a
randruseiko <randruseiko@n-ix.com>
2016-12-23 11:44:49 +0200

    Fixed memory issue on iPhone6+ by reducing image size

--- DKImagePickerController/View/DKAssetGroupGridLayout.swift
+++ DKImagePickerController/View/DKAssetGroupGridLayout.swift
@@ -9,23 +9,24 @@
 import UIKit
 
 open class DKAssetGroupGridLayout: UICollectionViewFlowLayout {
-	
-	open override func prepare() {
-		super.prepare()
-		
-		let interval: CGFloat = 4
-		self.minimumInteritemSpacing = interval
-		self.minimumLineSpacing = interval
-		
-		let contentWidth = self.collectionView!.bounds.width
-		
-		let itemCount = 3
-		var itemWidth = (contentWidth - interval * (CGFloat(itemCount) - 1)) / CGFloat(itemCount)
-		let actualInterval = (contentWidth - CGFloat(itemCount) * itemWidth) / (CGFloat(itemCount) - 1)
-		itemWidth += actualInterval - interval
-		
-		let itemSize = CGSize(width: itemWidth, height: itemWidth)
-		self.itemSize = itemSize
-	}
-	
+    
+    open override func prepare() {
+        super.prepare()
+        let minItemWidth: CGFloat = 100
+        
+        let interval: CGFloat = 3
+        self.minimumInteritemSpacing = interval
+        self.minimumLineSpacing = interval
+        
+        let contentWidth = self.collectionView!.bounds.width
+        
+        let itemCount = Int(floor(contentWidth / minItemWidth))
+        var itemWidth = (contentWidth - interval * (CGFloat(itemCount) - 1)) / CGFloat(itemCount)
+        let actualInterval = (contentWidth - CGFloat(itemCount) * itemWidth) / (CGFloat(itemCount) - 1)
+        itemWidth += actualInterval - interval
+        
+        let itemSize = CGSize(width: itemWidth, height: itemWidth)
+        self.itemSize = itemSize
+    }
+    
 }
